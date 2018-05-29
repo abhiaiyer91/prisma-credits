14659d88471ed4f308b79a3f0cdaecbcfd4df448
Abhi Aiyer <abhiaiyer91@gmail.com>
2018-05-28 21:18:07 -0700

    feat(purchase): purchase

--- src/index.js
+++ src/index.js
@@ -2,14 +2,26 @@ import { GraphQLServer } from "graphql-yoga";
 import { Prisma } from "prisma-binding";
 import resolvers from "./resolvers";
 
+function paymentBinding() {
+  return {
+    mutation: {
+      async sendPayment({ userId, paymentAmount, paymentMethod }) {
+        console.log(userId, paymentAmount, paymentMethod);
+        return true;
+      }
+    }
+  };
+}
+
 const server = new GraphQLServer({
   typeDefs: "src/schema/service-schema.graphql",
   resolvers,
   context: req => {
     const headers = req && req.request && req.request.headers;
     return {
-      isModerator: headers.role === 'MODERATOR',
+      isModerator: headers.role === "MODERATOR",
       userId: headers.userid,
+      paymentBinding: paymentBinding(),
       dataAccess: new Prisma({
         typeDefs: "src/generated/prisma.graphql",
         endpoint: "https://us1.prisma.sh/abhi-aiyer/credits/dev"
--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,3 +1,15 @@
+function totalPrice(skus) {
+  return (
+    skus &&
+    skus.reduce((memo, currentVal) => {
+      const count =
+        (currentVal && currentVal.price && currentVal.price.count) || 0;
+      memo += count;
+      return memo;
+    }, 0)
+  );
+}
+
 export default {
   Query: {
     getBalance: async (root, params, { dataAccess, userId }) => {
@@ -16,7 +28,18 @@ export default {
         throw new Error("User must be a moderator to add skus");
       }
 
-      return await dataAccess.mutation.createSku({ data: sku });
+      return await dataAccess.mutation.createSku(
+        { data: sku },
+        `{
+            id
+            name
+            description
+            price {
+              count
+              unit
+            }
+        }`
+      );
     },
     updateSku: async (root, { skuId, sku }, { dataAccess, isModerator }) => {
       if (!isModerator) {
@@ -56,6 +79,135 @@ export default {
     `;
 
       return await dataAccess.mutation.createPricingTable(input, fieldInfo);
+    },
+    purchase: async (
+      root,
+      { skuIds, description, paymentMethod },
+      { dataAccess, userId, paymentBinding }
+    ) => {
+      if (!userId) {
+        throw new Error("Must be logged in to purchase");
+      }
+      // 1. check to see if the skus exist
+
+      const skus = await dataAccess.query.skus(
+        {
+          where: {
+            name_in: skuIds
+          }
+        },
+        `{
+          id
+          name
+          description
+          price {
+            count
+            unit
+          }
+      }`
+      );
+
+      if (skus.length !== skuIds.length) {
+        throw new Error(
+          "There was an error processing some of your items, item may not exist"
+        );
+      }
+      // 2. check to see if we have a cardId or a token
+      const cardId = paymentMethod && paymentMethod.cardId;
+      const token = paymentMethod && paymentMethod.token;
+      let paymentMethodToUse;
+      // if we have a cardId use it, if we have token use it
+      if (!!cardId) {
+        paymentMethodToUse = cardId;
+      } else {
+        paymentMethodToUse = token;
+      }
+
+      if (!paymentMethodToUse) {
+        throw new Error("No valid payment method supplied to purchase");
+      }
+      // 3. Calculate the total price of what we are buying
+      const total = totalPrice(skus);
+      // 4. Process the payment with the payment service
+      const successfulPayment = await paymentBinding.mutation.sendPayment({
+        userId,
+        paymentAmount: total,
+        paymentMethod: paymentMethodToUse
+      });
+
+      if (!successfulPayment) {
+        throw new Error(
+          "Unable to process your payment, your payment credentials may have expired"
+        );
+      }
+      // 5. Create a finalized transaction
+
+      const transaction = {
+        description,
+        total: {
+          create: {
+            count: total,
+            unit: "US_CENTS"
+          }
+        },
+        initiatedBy: {
+          create: { userId }
+        },
+        statusMessage: "Credit Purchase Succeeded",
+        status: "SUCCEEDED",
+        type: "PURCHASE",
+        accountId: userId,
+        items: {
+          create: skus.map(sku => {
+            return {
+              sku: {
+                connect: {
+                  id: sku && sku.id,
+                  name: sku && sku.name
+                }
+              },
+              count: 1
+            };
+          })
+        }
+      };
+
+      await dataAccess.mutation.createTransaction({ data: transaction });
+      // 6. Increment the user's account balance
+
+      let account = await dataAccess.query.creditAccount({
+        where: {
+          userId
+        }
+      });
+
+      if (!account) {
+        await dataAccess.mutation.createCreditAccount({
+          data: {
+            userId,
+            balance: (skus && skus.length) || 0
+          }
+        });
+        account = await dataAccess.query.creditAccount({
+          where: {
+            userId
+          }
+        });
+      }
+
+      const updatedBalance = skus.length + ((account && account.balance) || 0);
+
+      await dataAccess.mutation.updateCreditAccount({
+        data: {
+          userId,
+          balance: updatedBalance
+        },
+        where: {
+          userId
+        }
+      });
+
+      return updatedBalance;
     }
   }
 };
